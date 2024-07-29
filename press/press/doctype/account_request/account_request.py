6f7ebc1fea5bbd2018e2565d599d29b13687c273
Abdislama Nor <me@engacs.pw>
2024-07-29 14:58:42 +0000

    FIxed Logo and Disabel Email when on developer_mode

--- press/press/doctype/account_request/account_request.py
+++ press/press/doctype/account_request/account_request.py
@@ -115,11 +115,11 @@ class AccountRequest(Document):
 	def send_verification_email(self):
 		url = self.get_verification_url()
 
-		if frappe.conf.developer_mode:
-			print(f"\nSetup account URL for {self.email}:")
-			print(url)
-			print()
-			return
+		# if frappe.conf.developer_mode:
+		# 	print(f"\nSetup account URL for {self.email}:")
+		# 	print(url)
+		# 	print()
+		# 	return
 
 		subject = "Verify your email for Frappe"
 		args = {}
@@ -135,13 +135,12 @@ class AccountRequest(Document):
 			if self.invited_by and self.role != "Press Admin":
 				subject = f"You are invited by {self.invited_by} to join Easytouch Cloud"
 				template = "invite_team_member"
-
+		logo = frappe.get_website_settings("app_logo") or frappe.get_hooks("app_logo_url")[-1]
 		args.update(
 			{
 				"invited_by": self.invited_by,
 				"link": url,
-				# "image_path": "/assets/press/images/frappe-logo-black.png",
-				"image_path": "https://github.com/frappe/gameplan/assets/9355208/447035d0-0686-41d2-910a-a3d21928ab94",
+				 "image_path": logo,
 				"read_pixel_path": get_url(
 					f"/api/method/press.utils.telemetry.capture_read_event?email={self.email}"
 				),
@@ -157,6 +156,7 @@ class AccountRequest(Document):
 			args=args,
 			now=True,
 		)
+		
 
 	def get_verification_url(self):
 		if self.saas:
