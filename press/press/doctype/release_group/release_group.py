d46b15d64a7a8c7dfe1e909912d049bbd326644d
Engacs <me@engacs.pw>
2024-08-15 15:21:47 +0000

    Fixed

--- press/press/doctype/release_group/release_group.py
+++ press/press/doctype/release_group/release_group.py
@@ -1108,9 +1108,8 @@ class ReleaseGroup(Document, TagHelpers):
 		if is_update:
 			update_rg_app_source(self, source)
 		else:
-      app_title = frappe.db.get_value("App Source", source.name, "app_title")
-      self.append("apps", {"title": app_title, "source": source.name, "app": source.app})
-
+			app_title = frappe.db.get_value("App Source", source.name, "app_title")
+			self.append("apps", {"title": app_title, "source": source.name, "app": source.app})
 		self.save()
 
 	def remove_app_if_invalid(self, source: "AppSource"):
